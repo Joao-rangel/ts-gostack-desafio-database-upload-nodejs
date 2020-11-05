c739b3b9371ed1ca988629212073f3ab065eb19d
Joao Rangel <joao.vran@gmail.com>
2020-11-05 16:38:25 -0300

    migration add category foreignKey to  Transactions

---
+++ src/database/migrations/1604604464579-AddCategoryForeignKeyToTransactions.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
+
+export default class AddCategoryForeignKeyToTransactions1604604464579
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'transactionCategory',
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropForeignKey('transactions', 'transactionCategory');
+  }
+}

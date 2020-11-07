f2361873839a62a927a14ec0713b56569840d612
Joao Rangel <joao.vran@gmail.com>
2020-11-07 13:50:04 -0300

    started import service

--- src/database/migrations/1604604464579-AddCategoryForeignKeyToTransactions.ts
+++ src/database/migrations/1604604464579-AddCategoryForeignKeyToTransactions.ts
@@ -2,7 +2,7 @@ import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
 
 export default class AddCategoryForeignKeyToTransactions1604604464579
   implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<any> {
+  public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createForeignKey(
       'transactions',
       new TableForeignKey({
@@ -16,7 +16,7 @@ export default class AddCategoryForeignKeyToTransactions1604604464579
     );
   }
 
-  public async down(queryRunner: QueryRunner): Promise<any> {
+  public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.dropForeignKey('transactions', 'transactionCategory');
   }
 }
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,55 @@
+import fs from 'fs';
+import path from 'path';
 import Transaction from '../models/Transaction';
+import FindOrCreateCategoryService from './FindOrCreateCategoryService';
 
 class ImportTransactionsService {
   async execute(): Promise<Transaction[]> {
-    // TODO
+    const filePath = path.resolve(__dirname, '..', '..', 'tmp', 'file.csv');
+    const fileText = fs.readFileSync(filePath, 'utf-8');
+
+    const [titles, ...rows] = fileText.split('\n');
+
+    const transactionKeys = titles.split(', ');
+
+    const objectTransactions = rows.map(row => {
+      const transactionValues = row.split(', ');
+      return transactionKeys.reduce((transaction: any, currentKey, index) => {
+        // eslint-disable-next-line no-param-reassign
+        transaction[currentKey] = transactionValues[index];
+
+        return transaction;
+      }, {});
+    });
+
+    const validTransactions = objectTransactions.filter(transaction => {
+      const { type } = transaction;
+      if (type === 'outcome' || type === 'income') {
+        return transaction;
+      }
+    });
+
+    const transactions = await Promise.all(
+      validTransactions.map(async validTransaction => {
+        const { title, type, value, category } = validTransaction;
+        const findOrCreateCategory = new FindOrCreateCategoryService();
+
+        const categoryObject = await findOrCreateCategory.execute({
+          category,
+        });
+
+        const transaction = {
+          title,
+          type,
+          value,
+          category_id: categoryObject.id,
+        } as Transaction;
+
+        return transaction;
+      }),
+    );
+
+    return transactions;
   }
 }
 
